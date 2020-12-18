8c3255a3dad5ba0d5062410c12b09e8d0c0dbe74
MatheusTrinca <matheusfelipetrinca@gmail.com>
2020-12-18 14:06:16 -0200

    Primeiro Commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur numquam eveniet quam 
+      tempora aliquam cum, veritatis doloribus iste praesentium, molestiae voluptatum omnis cumque 
+      asperiores sit quae ipsum placeat! Nihil, est.</p>
+    </section>
+  )
+}
+
+export default About
